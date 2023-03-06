98c7cb6c1df4b04e3e5fae500bc8d3cb6db7b349
Onur Abbasoglu <onurabbasoglu26@gmail.com>
2023-03-06 14:56:44 +0300

    Website started to be prepared.

---
+++ TechnologyStore/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechnologyStore.Repositories;
+
+namespace TechnologyStore.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        readonly ProductRepository productRepository = new ProductRepository();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Laptop()
+        {
+            var products = productRepository.GetAll();
+            return View(products);
+        }
+    }
+}
